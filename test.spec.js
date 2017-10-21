5fbb7a530f882199053f5900e127e96595b2d323
Travis Peterson <contact@travispeterson.us>
2017-10-21 14:16:16 -0500

    shims process.EventEmitter

--- test.spec.js
+++ test.spec.js
@@ -1,9 +1,10 @@
 const Couch = require('mock-couch');
+process.EventEmitter = require('events');
 
 describe('Some thing :', () => {
   it('should be true', () => {
     const couch = Couch.createServer()
-    couch.listen();
+    couch.listen(4465);
     expect(true).toBe(true);
     couch.close();
   });
